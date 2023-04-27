276699707dd0189f8f03ef816735dec606987f22
waltonklavierspieler <116521510+waltonklavierspieler@users.noreply.github.com>
2023-04-27 13:41:57 -0600

    Update .js

--- .js
+++ .js
@@ -1,1 +1,42 @@
+//arithmetic functions:
+function add(x, y) {
+	return x + y;
+}
+
+function subtract (x, y) {
+	return x - y;
+}
+
+function multiply (x, y) {
+	return x*y;
+}
+
+function divide (x, y) {
+	return x/y;
+}
+
+function exponent (x, y) {
+	var total = 1;
+	if (y > 0) {
+		for (var i = 1; i <= y; i++) {
+			total *= x;
+		}
+	}
+	
+	if (y < 0) {
+		for (var i = -1; i >= y; i--) {
+			total *= (1/x);
+		}
+	}
+	return total;
+}
+
+function sqrt (num) {
+	return Math.sqrt(num);
+}
+
+//operand function
+function operate (operand, x, y) {
+	return operand(x, y);
+}
 
